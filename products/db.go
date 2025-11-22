734993568d9ce0d0cc6740d67db6e75e996d9997
Achmad Rizki Ramadhan <achmadrizkiramadhan0101@gmail.com>
2025-11-22 21:00:46 +0700

    reset project, shibal

--- products/db.go
+++
@@ -1,8 +0,0 @@
-package products
-
-import "encore.dev/storage/sqldb"
-
-// Create the products database and assign it to the "db" variable
-var db = sqldb.NewDatabase("products", sqldb.DatabaseConfig{
-	Migrations: "./migrations",
-})
